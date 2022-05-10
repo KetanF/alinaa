702d412bc182820ba9b139c557f21fc757b6dd00
Ketan Foujdar <80333323+KetanF@users.noreply.github.com>
2022-05-10 11:05:17 +0530

    Add files via upload

--- alina.py
+++ alina.py
@@ -177,4 +177,4 @@ def output():
     return Response(stream_with_context(alinaapp()))
 
 if __name__ == '__main__':
-    app.run(debug=True, host ='0.0.0.0 ')
+    app.run(debug=False, host ='0.0.0.0 ')
